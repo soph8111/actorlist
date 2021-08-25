57b9b65880ed4367d4d0a9eee80c33f8cda69b33
Sophie Spang <soph8111@stud.kea.dk>
2021-08-25 16:41:19 +0200

    popup works

--- script.js
+++ script.js
@@ -4,34 +4,42 @@ const actorTemplate = document.querySelector("#name");
 const containerWithInfo = document.querySelector("#popup");
 const movieTemplate = document.querySelector("#info");
 
-// Henter actores ind ved hjælp af fetch (i en promise chain)
+// Henter actors ind ved hjælp af fetch (i en promise chain)
 async function getData(actorsFile) {
   const result = await fetch(actorsFile);
   const actors = await result.json();
 
-  console.log(actors); // tjekker i consollen om array er hentet
+  console.log(actors); // tjekker i konsollen om array er hentet
 
   showActors(actors); // kalder funktionen showActors og sender array'et med
 }
 
 function showActors(actors) {
+  //looper igennem array'et
   actors.forEach((actor) => {
-    //looper igennem array'et
     let klon = actorTemplate.cloneNode(true).content; // Gør det muligt at klone ned i template
     klon.querySelector(".name").textContent = actor.fullname; // navn hentes
     klon.querySelector(".name").addEventListener("click", () => {
       containerWithInfo.style.display = "block";
-      console.log(actor);
-      showInfo(actor);
+      console.log(actor); // viser den aktuelle skuespiller i konsollen
+      showInfo(actor); //sender den aktuelle skuespiller med ned i funktionen "showInfo"
     });
     containerWithActors.appendChild(klon); //kloner ind i sektionen
   });
 }
 
+//kører funktionen med den aktuelle skuespiller
 function showInfo(actor) {
+  containerWithInfo.innerHTML = '<div id="kryds"> X </div>'; //Reset'er sectionen, så der er plads til den nye film - krydset skal bruges til at kunne lukke boksen ned
   let klon = movieTemplate.cloneNode(true).content; // Gør det muligt at klone ned i template
-  klon.querySelector(".movie").textContent = actor.movie; // film hentes
+  klon.querySelector(".name").textContent = "Actor: " + actor.fullname;
+  klon.querySelector(".movie").textContent = "Movie: " + actor.movie; // film hentes på den aktuelle skuespiller
   containerWithInfo.appendChild(klon); //kloner ind i sektionen
+
+  //tilføjer eventlistener, så boksen fjerner sig, når man trykker på krydset
+  document.querySelector("#kryds").addEventListener("click", () => {
+    containerWithInfo.style.display = "none";
+  });
 }
 
 getData(actorsFile);
